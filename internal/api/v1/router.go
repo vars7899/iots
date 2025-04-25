8089256f72612cb728b50059d5dbec2955a9f35f
vaibhav dhiman <vars7899@gmail.com>
2025-04-25 15:42:25 +0530

    bug: updated stale access token generation

--- internal/api/v1/router.go
+++ internal/api/v1/router.go
@@ -10,11 +10,13 @@ import (
 	"go.uber.org/zap"
 )
 
-const API_V1_PREFIX = "/api/v1"
+// var AuthMiddleware
 
 func RegisterRoutes(e *echo.Echo, deps *di.Provider, baseLogger *zap.Logger) {
 	l := logger.Named(baseLogger, "v1Router")
-	r := api.NewAPIRouter(e, API_V1_PREFIX, baseLogger)
+	r := api.NewAPIRouter(e, string(api.ApiV1), baseLogger)
+
+	// authMiddleware := middleware.AuthRequired(e, deps.Helpers.TokenService, deps.Helpers.JTIService, deps.Services.CasbinService, l)
 
 	// Middleware
 	r.AddMiddleware(middleware.ErrorHandler(l))
@@ -27,6 +29,7 @@ func RegisterRoutes(e *echo.Echo, deps *di.Provider, baseLogger *zap.Logger) {
 	r.AddRoute(api.RouteConfig{
 		Prefix:  "/sensor",
 		Handler: handler.NewSensorHandler(deps, l),
+		// Middleware: []echo.MiddlewareFunc{middleware.JWT_JTI_Middleware(deps.Helpers.TokenService, deps.Helpers.JTIService, l)},
 	})
 	r.AddRoute(api.RouteConfig{
 		Prefix:  "/auth",
